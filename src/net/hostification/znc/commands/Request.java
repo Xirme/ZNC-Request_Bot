f905ed11bdfb7370aea20a690f29b2765c290696
Cyberpew <root@cyberpew.me>
2013-04-24 00:32:47 -0700

    Making progress. (Request.java)

--- src/net/hostification/znc/commands/Request.java
+++ src/net/hostification/znc/commands/Request.java
@@ -1,21 +1,23 @@
 package net.hostification.znc.commands;
 
 import net.hostification.znc.znchelper.ZNCHelper;
+import net.hostification.znc.znchelper.Config;
 
 import org.pircbotx.User;
 import org.pircbotx.hooks.events.MessageEvent;
 
+@SuppressWarnings("rawtypes")
 public class Request {
 	
 	public void onMessage(MessageEvent event) throws Exception {
 		if (event.getMessage().startsWith("!request")) {
 			String[] arguments = event.getMessage().split(" ");
-			if(arguments.length == 0){
-				ZNCHelper.bot.sendMessage(event.getUser(), "Invalid syntax! ");
-				ZNCHelper.bot.sendMessage(event.getUser(), "Valid syntax is: !request <username> <email> <irc.server> <irc.port>");
-			}
 			if(arguments.length == 4){
 				//if all args filled correctly, then insert the data in to MySQL database and PM ZNC administrator with notification of new ZNC user request. 
+				ZNCHelper.bot.sendMessage(Config.admins, "Success!"); 
+			}else{
+				ZNCHelper.bot.sendMessage(event.getUser(), "Invalid syntax! ");
+				ZNCHelper.bot.sendMessage(event.getUser(), "Valid syntax is: !request <username> <email> <irc.server> <irc.port>");
 			}
 		}
 	}
