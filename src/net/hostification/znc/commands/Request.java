5f7a023d4eda587e65fbea58b2e194ebf3d945ed
Cyberpew <root@cyberpew.me>
2013-04-29 14:34:14 -0700

    Array complete. (hopefully)

--- src/net/hostification/znc/commands/Request.java
+++ src/net/hostification/znc/commands/Request.java
@@ -17,19 +17,25 @@ public class Request extends ListenerAdapter {
 		cSyntax = new String[4];
 		
 		cSyntax = event.getMessage().split("\\s+");
-		
-		if (event.getMessage().startsWith("!request")) {
+		if (cSyntax.length == 4) {
+			if (event.getMessage().startsWith("!request")) {
 				//if all args filled correctly, then insert the data in to MySQL database and PM ZNC administrator with notification of new ZNC user request. 
 				//Syntax is: !request <username> <email> <irc.server> <irc.port>
 			
 			
 				for (int i = 0; i < Config.admins.length; i++) {
 					ZNCHelper.bot.sendMessage(Config.admins[i], "Success!"); 
-					ZNCHelper.bot.sendMessage(Config.admins[i], "Data: " + cSyntax[4]);
+					ZNCHelper.bot.sendMessage(Config.admins[i], "Data: " + cSyntax);
 				}
-			}else{
+			}
+			if (cSyntax.length < 4) {
+				ZNCHelper.bot.sendMessage(event.getUser(), "Invalid syntax! ");
+				ZNCHelper.bot.sendMessage(event.getUser(), "Valid syntax is: !request <username> <email> <irc.server> <irc.port>");
+			}
+			if (cSyntax.length > 4) {
 				ZNCHelper.bot.sendMessage(event.getUser(), "Invalid syntax! ");
 				ZNCHelper.bot.sendMessage(event.getUser(), "Valid syntax is: !request <username> <email> <irc.server> <irc.port>");
 			}
 		}
 	}
+}
